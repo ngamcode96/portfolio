d0d60fe37134e5ee38054644df8f38baea78d64e
ngamcode96 <ngamcode@gmail.com>
2021-12-29 22:08:41 +0100

    Ajout de contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\User;
+use App\Form\ContactType;
 use App\Repository\CompetencesRepository;
 use App\Repository\RealisationRepository;
 use App\Repository\UserRepository;
@@ -16,17 +20,29 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(UserRepository $userRepository, RealisationRepository $realisationRepository, CompetencesRepository $competencesRepository): Response
+    public function index(UserRepository $userRepository, RealisationRepository $realisationRepository, CompetencesRepository $competencesRepository, Request $request): Response
     {
         $user = $userRepository->findAll(); 
         $realisations = $realisationRepository->findAll(); 
         $competences = $competencesRepository->findAll();
 
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            dump($contact);
+            die();
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'user' => $user[0],
             'realisations'=>$realisations,
-            'competences'=>$competences
+            'competences'=>$competences,
+            'form'=>$form->createView()
         ]);
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+
+
+class Contact
+{
+  
+    private $id;
+
+   
+    private $name;
+
+  
+    private $sender;
+
+    private $subject;
+
+ 
+    private $message;
+
+  
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSender(): ?string
+    {
+        return $this->sender;
+    }
+
+    public function setSender(string $sender): self
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
